38b373c543a287a854039bdf399f230eb63da6ed
Sinan49300 <sinan49@live.fr>
2020-06-16 18:02:53 +0200

    6 files modified, 9 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartService } from './cart.service';
+import { CartComponent } from './cart/cart.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   imports: [ 
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
-      { path: 'products/:productId', component: ProductDetailsComponent }
+      { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'cart', component: CartComponent },
+      { path: 'shipping', component: ShippingComponent },
     ])
   ],
   declarations: [ 
@@ -25,7 +34,12 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     ProductListComponent,
     TopBarComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent ],
-  bootstrap:    [ AppComponent ]
+    ProductDetailsComponent,
+    CartComponent,
+    ShippingComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [CartService]
 })
 export class AppModule { }
+
+registerLocaleData(localeFr, 'fr-FR');
